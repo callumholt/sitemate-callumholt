befce6ad82433a38ef2c5de6d9fb9b3f8b44ad12
callumholt <callumpholt@gmail.com>
2024-06-18 09:34:15 +1000

    manually tested CRUD, modified GUI to have manual retreival, improved presentation of GUI

--- frontend/issue_tracker/src/components/IssueList.js
+++ frontend/issue_tracker/src/components/IssueList.js
@@ -3,20 +3,26 @@ import React, { useEffect, useState } from "react";
 const IssueList = () => {
   const [issues, setIssues] = useState([]);
 
-  useEffect(() => {
+  const fetchIssues = () => {
     fetch("http://127.0.0.1:5000/issues")
       .then((response) => response.json())
       .then((data) => setIssues(data))
       .catch((error) => console.error("Error fetching issues:", error));
+  };
+
+  useEffect(() => {
+    fetchIssues();
   }, []);
 
   return (
     <div>
       <h2>Issues</h2>
+      <button onClick={fetchIssues}>Retrieve Issues</button>
       <ul>
         {issues.map((issue) => (
           <li key={issue.id}>
-            {issue.title}: {issue.description}
+            id: {issue.id} - title: {issue.title} - description:{" "}
+            {issue.description}
           </li>
         ))}
       </ul>
